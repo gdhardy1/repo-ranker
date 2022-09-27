4813ccd48b8b5431c45313c12cc9150f2dee4dfa
gdhardy1 <47133442+gdhardy1@users.noreply.github.com>
2022-09-27 16:03:13 -0400

    add graphql queries

---
+++ src/util/graphql/fragments.ts
@@ -0,0 +1,53 @@
+import { gql } from "@apollo/client";
+
+const FRAGMENT_REPOSITORY = gql`
+  fragment repository on Repository {
+    id
+    name
+    defaultBranchRef {
+      name
+    }
+    stargazers {
+      totalCount
+    }
+  }
+`;
+export const FRAGMENT_REPOSITORY_CONNECTION = gql`
+  fragment repositoryConnection on RepositoryConnection {
+    edges {
+      node {
+        ...repository
+      }
+    }
+    totalCount
+    pageInfo {
+      startCursor
+      endCursor
+      hasNextPage
+      hasPreviousPage
+    }
+  }
+
+  ${FRAGMENT_REPOSITORY}
+`;
+
+export const FRAGMENT_COMMIT_HISTORY = gql`
+  fragment commits on Commit {
+    history(first: 10) {
+      totalCount
+      edges {
+        node {
+          id
+          message
+          url
+        }
+      }
+      pageInfo {
+        startCursor
+        endCursor
+        hasNextPage
+        hasPreviousPage
+      }
+    }
+  }
+`;
---
+++ src/util/graphql/queries.ts
@@ -0,0 +1,36 @@
+import { gql } from "@apollo/client";
+
+import {
+  FRAGMENT_COMMIT_HISTORY,
+  FRAGMENT_REPOSITORY_CONNECTION,
+} from "./fragments";
+
+export const GET_ORGANIZATON = gql`
+  query Organization($login: String!, $cursor: String) {
+    organization(login: $login) {
+      id
+      name
+      login
+      repositories(
+        first: 10
+        after: $cursor
+        orderBy: { field: STARGAZERS, direction: DESC }
+      ) {
+        ...repositoryConnection
+      }
+    }
+  }
+  ${FRAGMENT_REPOSITORY_CONNECTION}
+`;
+
+export const GET_REPOSITORY = gql`
+  query Repository($login: String!, $repoName: String!, $branch: String!) {
+    repository(name: $repoName, owner: $login) {
+      object(expression: $branch) {
+        ...commits
+      }
+    }
+  }
+
+  ${FRAGMENT_COMMIT_HISTORY}
+`;
