47c92811ed8f842f6af65c9ac302225500f1db0f
gdhardy1 <47133442+gdhardy1@users.noreply.github.com>
2022-09-27 17:15:34 -0400

    add apollo client and browser router providers

---
+++ src/components/Search.tsx
@@ -0,0 +1,71 @@
+import React, { FormEvent, useState, useContext, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import { useOrganizationData } from "../util/hooks/useOrganizationData";
+import { AppContext } from "../App";
+
+export default function SearchBox(props: any) {
+  const [searchText, setSearchText] = useState("");
+  const [getOrganizationData] = useOrganizationData();
+  const navigate = useNavigate();
+  const { setNextCursor } = useContext(AppContext);
+
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+    if (searchText === "") {
+      alert("you must provide a org login");
+      return;
+    }
+    setNextCursor(undefined);
+    let result = await getOrganizationData({ login: searchText });
+
+    if (result?.data?.organization) {
+      setSearchText("");
+      navigate(`/${searchText}`);
+    } else {
+      alert("Organization not found");
+    }
+  };
+  return (
+    <AppContext.Consumer>
+      {(context) => (
+        <>
+          <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
+            <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+              <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+                <form className="space-y-6" onSubmit={handleSubmit}>
+                  <div>
+                    <label
+                      htmlFor="email"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Search for a Github organization by login.
+                    </label>
+                    <div className="mt-1">
+                      <input
+                        id="github-org"
+                        type="text"
+                        value={searchText}
+                        onChange={(e) => setSearchText(e.target.value)}
+                        placeholder="Netflix"
+                        className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                      />
+                    </div>
+                  </div>
+
+                  <div>
+                    <button
+                      type="submit"
+                      className="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                    >
+                      Search
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+    </AppContext.Consumer>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,51 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./style/index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+
+import { setContext } from "@apollo/client/link/context";
+import { memoryUsage } from "process";
+
+const httpLink = createHttpLink({ uri: "https://api.github.com/graphql" });
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      authorization: `Bearer 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`,
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <RouterProvider router={router} />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
