fca9df9d7866d812a39f4fa04d8812b005d2838f
gdhardy1 <47133442+gdhardy1@users.noreply.github.com>
2022-10-01 18:45:36 -0400

    refactor: open commits in new tab

--- src/components/Commits/index.tsx
+++ src/components/Commits/index.tsx
@@ -1,7 +1,5 @@
-import React, { useContext, useEffect } from "react";
-
+import React, { useContext } from "react";
 import { AppContext } from "../../App";
-import { useRepositoryData } from "../../util/hooks/useRepositoryData";
 
 export default function Commits(props: any) {
   const { repositoryData } = useContext(AppContext);
@@ -12,7 +10,12 @@ export default function Commits(props: any) {
         const { node: commit } = edge;
         return (
           <li key={commit.id}>
-            <a href={commit.url} className="block hover:bg-gray-50">
+            <a
+              href={commit.url}
+              rel="noreferrer noopener"
+              target="_blank"
+              className="block hover:bg-gray-50"
+            >
               <div className="px-4 py-4 sm:px-6">
                 <div className="flex items-center justify-between">
                   <p className="truncate text-sm font-medium text-indigo-600">
